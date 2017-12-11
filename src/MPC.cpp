9baf4eacbea0c11b283b4dc1e68f107c94a772f9
Clifton Pereira <pereiraclifton@gmail.com>
2017-12-11 10:01:23 -0500

    Fixed mistake from lecture notes

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -117,7 +117,7 @@ class FG_eval {
 	  fg[1 + cte_start + t] =
 		  cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
 	  fg[1 + epsi_start + t] =
-		  epsi1 - ((psi0 - psides0) + (v0/Lf * delta0 * dt));
+		  epsi1 - ((psi0 - psides0) - (v0/Lf * delta0 * dt));
 	}
 
   }
