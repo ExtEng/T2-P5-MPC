cd46183209fed1e62bec4a9c074acfd3cb641925
Clifton Pereira <pereiraclifton@gmail.com>
2017-12-10 22:46:19 -0500

    test

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -57,14 +57,14 @@ class FG_eval {
 
     // Minimize the use of actuators.
     for (int t = 0; t < N - 1; t++) {
-      fg[0] += 5*CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 20*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 20*CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
-      fg[0] += 200*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-      fg[0] += 10*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 3000*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 750*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 	
 	fg[1 + x_start] = vars[x_start];
