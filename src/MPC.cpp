d2cb1749a9c2122899f6d7eae7fc0e186fea168e
Clifton Pereira <pereiraclifton@gmail.com>
2017-12-11 21:46:23 -0500

    passed, but a little unstable

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -65,7 +65,7 @@ class FG_eval {
 
     // Minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
-      fg[0] += 2200*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 3000*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += 750*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 	
