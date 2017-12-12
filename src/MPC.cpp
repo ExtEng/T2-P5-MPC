b89d538ef59b31f2b7fdc8b174a3a2ec35850436
Clifton Pereira <pereiraclifton@gmail.com>
2017-12-11 21:42:37 -0500

    passed 3 laps, speed around corner 20~, uping speed

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -52,7 +52,7 @@ class FG_eval {
 	for (int t = 0; t < N; t++) {
       fg[0] += 3000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
       fg[0] += 3000*CppAD::pow(vars[epsi_start + t]- ref_epsi, 2);
-      fg[0] += 3*CppAD::pow(vars[v_start + t] - ref_v, 2);
+      fg[0] += 3.5*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
